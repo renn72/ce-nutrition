b2b9e6368b98b907661cdccd7e83c3d5d8220529
renn <renn@warner.systems>
2024-12-22 14:19:16 +1100

    camera switch

--- src/app/user/cam/page.tsx
+++ src/app/user/cam/page.tsx
@@ -4,37 +4,43 @@ import { useCallback, useEffect, useState } from 'react'
 
 import Webcam from 'react-webcam'
 
+import { Button } from '@/components/ui/button'
+
 export default function Page() {
   const [deviceId, setDeviceId] = useState({})
   const [devices, setDevices] = useState([])
 
-  const handleDevices = useCallback(
-    // @ts-ignore
-    (mediaDevices) =>
-      // @ts-ignore
-      setDevices(mediaDevices.filter(({ kind }) => kind === 'videoinput')),
-    [setDevices],
-  )
+  // const handleDevices = useCallback(
+  //   // @ts-ignore
+  //   (mediaDevices) =>
+  //     // @ts-ignore
+  //     setDevices(mediaDevices.filter(({ kind }) => kind === 'videoinput')),
+  //   [setDevices],
+  // )
+  //
+  // useEffect(() => {
+  //   navigator.mediaDevices.enumerateDevices().then(handleDevices)
+  // }, [handleDevices])
 
-  useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then(handleDevices)
-  }, [handleDevices])
+  const videoConstraints = {
+    facingMode: 'user',
+  }
+
+  const handleClick = () => {
+    if (videoConstraints.facingMode === 'user') {
+      videoConstraints.facingMode = 'environment'
+    } else {
+      videoConstraints.facingMode = 'user'
+    }
+  }
 
   return (
-    <div className='p-4'>
-      {devices.map((device, key) => (
-        <div>
-          <Webcam
-            audio={false}
-            // @ts-ignore
-            videoConstraints={{ deviceId: device.deviceId }}
-          />
-          {
-            // @ts-ignore
-            device.label || `Device ${key + 1}`
-          }
-        </div>
-      ))}
+    <div className='p-4 flex flex-col  gap-4 mt-10'>
+      <Webcam
+        audio={false}
+        videoConstraints={videoConstraints}
+      />
+      <Button onClick={handleClick}>Change</Button>
     </div>
   )
 }
