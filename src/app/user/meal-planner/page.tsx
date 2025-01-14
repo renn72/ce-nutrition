d286906e3d184b8b145901eabf8fb6a2e2d78e97
renn <renn@warner.systems>
2025-01-14 15:43:43 +1100

    planner tweak

--- src/app/user/meal-planner/page.tsx
+++ src/app/user/meal-planner/page.tsx
@@ -7,18 +7,27 @@ import { useState } from 'react'
 import { GetAllDailyLogs, GetUserById, UserPlan } from '@/types'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
+import { Button } from '@/components/ui/button'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
 const Day = ({
   date,
   plan,
   dailyLogs,
   userId,
+  index,
 }: {
   date: Date
   dailyLogs: GetAllDailyLogs | null | undefined
   plan: UserPlan
   userId: string
+  index: number
 }) => {
   const ctx = api.useUtils()
   const { mutate: createDailyLog } = api.dailyLog.create.useMutation({
@@ -38,35 +47,52 @@ const Day = ({
           weekday: 'long',
           day: 'numeric',
         })}
-        <div>
-          {plan?.userMeals.map((meal) => {
-            const recipes = plan.userRecipes.filter(
-              (recipe) => recipe.mealIndex == meal.mealIndex,
-            )
-            return (
-              <div
-                key={meal.id}
-                className='flex gap-2 flex-col'
-              >
-                <div className='text-sm text-muted-foreground font-medium'>
-                  {meal.mealTitle}
-                </div>
-                <ToggleGroup type='single'>
+      </div>
+      <div className='flex gap-2 flex-col'>
+        {plan?.userMeals.map((meal) => {
+          const recipes = plan.userRecipes.filter(
+            (recipe) => recipe.mealIndex == meal.mealIndex,
+          )
+          return (
+            <div
+              key={meal.id}
+              className='flex gap-6 flex items-center'
+            >
+              <div className='text-sm text-muted-foreground font-medium'>
+                {meal.mealTitle}
+              </div>
+
+              <Select>
+                <SelectTrigger className='w-[180px] h-7 bg-background'>
+                  <SelectValue placeholder='Meal' />
+                </SelectTrigger>
+                <SelectContent>
                   {recipes.map((recipe) => (
-                    <ToggleGroupItem
+                    <SelectItem
                       key={recipe.id}
                       value={recipe.id.toString()}
                       className='text-xs py-1 px-1'
                     >
                       {recipe.name}
-                    </ToggleGroupItem>
+                    </SelectItem>
                   ))}
-                </ToggleGroup>
-              </div>
-            )
-          })}
-        </div>
+                </SelectContent>
+              </Select>
+            </div>
+          )
+        })}
       </div>
+      {index === 0 ? (
+        <div className='flex gap-2 justify-end'>
+          <Button
+            variant='ghost'
+            size='sm'
+            className='text-xs'
+          >
+            Apply to all week
+          </Button>
+        </div>
+      ) : null}
     </div>
   )
 }
@@ -94,6 +120,7 @@ const DayList = ({
         return (
           <Day
             key={i}
+            index={i}
             date={date}
             dailyLogs={dailyLogs}
             plan={currentUserPlan}
@@ -121,7 +148,7 @@ export default function Home() {
   if (!currentUser) return null
 
   return (
-    <div className='flex flex-col gap-2 w-full text-sm mt-16'>
+    <div className='flex flex-col gap-2 w-full text-sm my-16'>
       <div className='flex gap-2 justify-between px-6 bg-secondary w-full py-2 items-end text-base font-semibold text-muted-foreground '>
         <ChevronLeft
           onClick={() =>
