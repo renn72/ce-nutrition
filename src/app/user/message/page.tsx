f63ef9a6554c7394eb58746f916224adc89dcb18
renn <renn@warner.systems>
2025-03-02 13:06:03 +1100

    base user list

--- src/app/user/message/page.tsx
+++ src/app/user/message/page.tsx
@@ -2,10 +2,23 @@
 
 import { api } from '@/trpc/react'
 
-import { impersonatedUserAtom } from '@/atoms'
-import { useAtomValue } from 'jotai'
-import { CirclePlus } from 'lucide-react'
+import { useState } from 'react'
 
+import { impersonatedUserAtom } from '@/atoms'
+import { cn } from '@/lib/utils'
+import { GetAllUsers } from '@/types'
+import { useAtomValue } from 'jotai'
+import { Check, ChevronsUpDown, CirclePlus } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
 import {
   Dialog,
   DialogContent,
@@ -14,22 +27,96 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 
-const NewMessage = () => {
+const SendTo = ({ sendList, value, setValue }: { sendList: GetAllUsers | null | undefined, value: string, setValue: (value: string) => void }) => {
+  const [open, setOpen] = useState(false)
+  if (!sendList) return null
   return (
-    <Dialog>
+    <Popover
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <PopoverTrigger asChild>
+        <Button
+          variant='outline'
+          role='combobox'
+          aria-expanded={open}
+          className='w-[200px] justify-between'
+        >
+          {value
+            ? sendList.find((user) => user.id === value)?.name
+            : 'Select recipient'}
+          <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-[200px] p-0'>
+        <Command>
+          <CommandInput placeholder='Search...' />
+          <CommandList>
+            <CommandEmpty>No one found.</CommandEmpty>
+            <CommandGroup>
+              {sendList.map((user) => (
+                <CommandItem
+                  key={user.id}
+                  value={user.id}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? '' : currentValue)
+                    setOpen(false)
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      'mr-2 h-4 w-4',
+                      value === user.id ? 'opacity-100' : 'opacity-0',
+                    )}
+                  />
+                  {user.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+const NewMessage = ({
+  sendList,
+}: {
+  sendList: GetAllUsers | null | undefined
+}) => {
+  const [message, setMessage] = useState('')
+  const [subject, setSubject] = useState('')
+  const [recipient, setRecipient] = useState<string>('')
+
+  return (
+    <Dialog
+
+    >
       <DialogTrigger asChild>
         <CirclePlus
           size={24}
           className='cursor-pointer'
         />
       </DialogTrigger>
-      <DialogContent className=''>
+      <DialogContent
+        onOpenAutoFocus={(e) => e.preventDefault()}
+        className=''>
         <DialogHeader>
           <DialogTitle>New Message</DialogTitle>
-          <DialogDescription>Write your message here</DialogDescription>
+          <DialogDescription></DialogDescription>
+          <SendTo
+            sendList={sendList}
+            value={recipient}
+            setValue={setRecipient}
+          />
         </DialogHeader>
-        <DialogDescription>Write your message here</DialogDescription>
       </DialogContent>
     </Dialog>
   )
@@ -46,9 +133,18 @@ export default function Message() {
   const { data: getAllUser, isLoading: getAllUserLoading } =
     api.user.getAll.useQuery()
 
+  if (!currentUser) return null
+  if (
+    messagesLoading ||
+    sendMessageLoading ||
+    currentUserLoading ||
+    getAllUserLoading
+  )
+    return null
   const sendList = getAllUser?.filter((user) => {
     if (currentUser.isRoot) return true
     if (currentUser.isCreator) return true
+    if (currentUser.id === user.id) return false
     if (currentUser.isTrainer) return true
 
     if (user.isTrainer) return true
@@ -61,7 +157,7 @@ export default function Message() {
     <div className='flex gap-4 flex-col items-center justify-center w-full min-h-[200px]'>
       <div className='flex gap-4'>
         <h1>Messages</h1>
-        <NewMessage />
+        <NewMessage sendList={sendList} />
       </div>
     </div>
   )
