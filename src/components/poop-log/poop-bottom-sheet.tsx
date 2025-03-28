68038ff25ff842e7c72b2b994506e4bed2b05a5f
renn <renn@warner.systems>
2025-03-28 13:31:12 +1100

    funny

--- src/components/poop-log/poop-bottom-sheet.tsx
+++ src/components/poop-log/poop-bottom-sheet.tsx
@@ -1,9 +1,46 @@
 'use client'
 
+import { useState } from 'react'
+
 import { GetDailyLogById } from '@/types'
 import NumberFlow from '@number-flow/react'
 import { Sheet } from '@silk-hq/components'
-import { ChevronDown, CircleX, ListCollapse, Trash, Trash2 } from 'lucide-react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { ChevronDown, ListCollapse, Shuffle, Trash2 } from 'lucide-react'
+const transition = {
+  duration: 0.15,
+  ease: 'easeInOut',
+}
+
+const slideAnimation = {
+  initial: { y: 20, opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  exit: { y: -20, opacity: 0 },
+  transition: {transition},
+}
+
+const toiletPhrases = [
+  'Dropping the kids off',
+  'Taking the Browns out',
+  'Downloading some logs',
+  'Releasing the Kraken',
+  'Visiting Uncle John',
+  'Answering nature’s call',
+  'Launching a torpedo',
+  'Sending a fax out',
+  'Making a splash',
+  'Delivering a package',
+  'On the porcelain throne',
+  'Filling the royal seat',
+  'Deploying the troops',
+  'Meeting the plumbing team',
+  'Evacuating the floor',
+  'Taking the Oval seat',
+  'On a secret mission',
+  'Handling some business',
+  'Unloading the cargo',
+  'Sinking the Titanic',
+]
 
 const PoopBottomSheet = ({
   todaysDailyLog,
@@ -14,6 +51,7 @@ const PoopBottomSheet = ({
   totalPoop: number
   deletePoopLog: ({ id }: { id: number }) => void
 }) => {
+  const [title, setTitle] = useState('Toilet Log')
   return (
     <Sheet.Root license='non-commercial'>
       <Sheet.Trigger className='flex gap-2 items-center justify-center absolute  top-10 right-0'>
@@ -23,8 +61,21 @@ const PoopBottomSheet = ({
         />
       </Sheet.Trigger>
       <Sheet.Portal>
-        <Sheet.View className='z-[999] h-[100vh] bg-primary/20 '>
-          <Sheet.Content className='min-h-[200px] max-h-[80vh] h-full rounded-t-3xl bg-background'>
+        <Sheet.View className='z-[999] h-[100vh] bg-black/50 '>
+          <Sheet.Content className='min-h-[200px] max-h-[80vh] h-full rounded-t-3xl bg-background relative'>
+            <div className='absolute bottom-10 right-0 p-2 text-primary'>
+              <Shuffle
+                size={16}
+                className='text-primary/30 active:scale-90 transition-transform cursor-pointer'
+                onClick={(e) => {
+                  e.stopPropagation()
+                  e.preventDefault()
+                  const rnd = Math.floor(Math.random() * toiletPhrases.length)
+                  const phrase = toiletPhrases[rnd] ?? 'Toilet Log'
+                  setTitle(phrase)
+                }}
+              />
+            </div>
             <div className='flex flex-col justify-between h-full'>
               <div className='flex flex-col '>
                 <div className='flex justify-center pt-1'>
@@ -33,12 +84,27 @@ const PoopBottomSheet = ({
                     action='dismiss'
                   />
                 </div>
-                <div className='flex gap-4 pt-4 border-b-[1px] border-primary pb-4 relative'>
-                  <Sheet.Title className='text-lg ml-4'>Toilet Log</Sheet.Title>
+                <div className='flex gap-4 pt-4 border-b-[1px] border-primary pb-4 relative font-medium'>
+                  <div className='transition-transform '>
+                    <Sheet.Title className='text-lg ml-4 '>
+                      <AnimatePresence mode='wait'>
+                        <motion.div
+                          key={title}
+                          className=''
+                          initial={slideAnimation.initial}
+                          animate={slideAnimation.animate}
+                          exit={slideAnimation.exit}
+                          transition={slideAnimation.transition}
+                        >
+                          {title}
+                        </motion.div>
+                      </AnimatePresence>
+                    </Sheet.Title>
+                  </div>
                   <Sheet.Description className='hidden'>
                     Toilet Log
                   </Sheet.Description>
-                  <div className='border w-px h-6'/>
+                  <div className='border w-px h-6' />
                   <NumberFlow
                     value={totalPoop}
                     className='text-lg text-primary ml-2 '
@@ -62,16 +128,16 @@ const PoopBottomSheet = ({
                           {poopLog.createdAt.toLocaleTimeString('en-AU')}
                         </div>
                         <div className='justify-self-end'>
-                        <Trash2
-                          size={16}
-                          className='cursor-pointer text-destructive hover:text-primary active:scale-90 transition-transform cursor-pointer mb-[1px]'
-                          onClick={() => {
-                            deletePoopLog({
-                              id: poopLog.id,
-                            })
-                          }}
-                        />
-                      </div>
+                          <Trash2
+                            size={16}
+                            className='cursor-pointer text-destructive hover:text-primary active:scale-90 transition-transform cursor-pointer mb-[1px]'
+                            onClick={() => {
+                              deletePoopLog({
+                                id: poopLog.id,
+                              })
+                            }}
+                          />
+                        </div>
                       </div>
                     ))
                   )}
