ea43ecd47d2627c29790a8c16ee82975309d5262
renn <renn@warner.systems>
2025-05-08 12:34:50 +1000

    weight modal update

--- src/app/admin/user-info/user-info.tsx
+++ src/app/admin/user-info/user-info.tsx
@@ -4,158 +4,13 @@ import { api } from '@/trpc/react'
 
 import { cn } from '@/lib/utils'
 import { GetAllDailyLogs, GetUserById } from '@/types'
-import { ArrowDown, ArrowUp } from 'lucide-react'
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  Line,
-  LineChart,
-  XAxis,
-  YAxis,
-} from 'recharts'
 
-import {
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-  type ChartConfig,
-} from '@/components/ui/chart'
+import { UserWeight } from './user-weight'
 
-const chartConfig = {
-  weight: {
-    label: 'Weight',
-    color: '#2563eb',
-  },
-} satisfies ChartConfig
-
-type Log = {
-  date: Date
-  weight: number
-}
-
-const BodyWeightChart = ({ logs }: { logs: Log[] }) => {
-  const dataMin = Math.min(...logs.map((d) => Number(d.weight))) - 0.1
-  const dataMax = Math.max(...logs.map((d) => Number(d.weight))) + 0.1
-  return (
-    <ChartContainer
-      config={chartConfig}
-      className='w-full min-h-[80px] max-h-[250px]'
-    >
-      <LineChart data={logs}>
-        <CartesianGrid vertical={false} />
-        <XAxis
-          dataKey='date'
-          tickLine={false}
-          tickMargin={0}
-          axisLine={false}
-        />
-        <YAxis
-          orientation='right'
-          width={0}
-          allowDecimals={true}
-          padding={{ top: 0, bottom: 0 }}
-          interval='preserveStartEnd'
-          dataKey='weight'
-          tickLine={false}
-          tickCount={10}
-          tickMargin={0}
-          axisLine={false}
-          type='number'
-          allowDataOverflow={false}
-          domain={[dataMin, dataMax]}
-        />
-        <Line
-          dataKey='weight'
-          dot={false}
-          strokeWidth={1}
-          type='monotone'
-          isAnimationActive={true}
-        />
-        <ChartTooltip content={<ChartTooltipContent />} />
-      </LineChart>
-    </ChartContainer>
-  )
-}
-
-const WeightArrow = ({ weight, lastWeight }: { weight: number | null, lastWeight: number | null }) => {
-  if (!weight) return null
-  if (!lastWeight) return null
-  return (
-    <>
-      {weight > lastWeight ? (
-        <ArrowUp
-          className='text-secondary-foreground'
-          size={20}
-        />
-      ) : (
-        <ArrowDown
-          className='text-accent-foreground'
-          size={20}
-        />
-      )}
-    </>
-  )
-}
-
-const UserWeight = ({
-  user,
-  dailyLogs,
-}: {
-  user: GetUserById
-  dailyLogs: GetAllDailyLogs
-}) => {
-  const logs = dailyLogs
-    .filter((dailyLog) => dailyLog.morningWeight !== '')
-    .filter((dailyLog) => dailyLog.morningWeight)
-    .map((dailyLog) => {
-      const date = new Date(dailyLog.date)
-      return {
-        date: date,
-        weight: Number(dailyLog.morningWeight),
-      }
-    })
-    .sort((a, b) => b.date.getTime() - a.date.getTime())
-    .slice(0, 7)
-
-  const weight: number | null = logs.reduce<number | null>((acc, cur) => {
-    if (cur.weight && acc === null) {
-      return Number(cur.weight)
-    }
-    return acc
-  }, null)
-
-  const lastWeight: number | null = logs
-    .reverse()
-    .reduce<number | null>((acc, cur) => {
-      if (cur.weight && acc === null) {
-        return Number(cur.weight)
-      }
-      return acc
-    }, null)
-
+const UserGoals = () => {
   return (
     <div className='border rounded-lg p-4 flex flex-col w-[300px] items-center'>
-      <div
-        className={cn(
-          'flex items-center justify-center rounded-full border-8 border-blue-600/30',
-          ' w-56 h-56 text-4xl font-bold text-primary/90',
-        )}
-      >
-        <div className='flex items-baseline'>
-          {weight}
-          {weight ? (
-            <span className='text-xl text-muted-foreground'>kg</span>
-          ) : (
-            ''
-          )}
-          <WeightArrow
-            weight={weight}
-            lastWeight={lastWeight}
-          />
-        </div>
-      </div>
-      <BodyWeightChart logs={logs} />
+      goals
     </div>
   )
 }
@@ -167,11 +22,12 @@ const UserInfo = ({ userId }: { userId: string }) => {
   if (!user) return null
   if (!dailyLogs) return null
   return (
-    <div className='p-4'>
+    <div className='p-4 flex flex-wrap gap-4 w-full'>
       <UserWeight
         user={user}
         dailyLogs={dailyLogs}
       />
+      <UserGoals />
     </div>
   )
 }
---
+++ src/server/api/routers/goals.ts
@@ -0,0 +1,74 @@
+import { createTRPCRouter, protectedProcedure } from '~/server/api/trpc'
+import { client, db } from '~/server/db'
+import { log } from '~/server/db/schema/log'
+import { goals, user, userSettings } from '~/server/db/schema/user'
+import { eq } from 'drizzle-orm'
+import { z } from 'zod'
+
+export const goalsRouter = createTRPCRouter({
+  getUser: protectedProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const res = await ctx.db.query.goals.findMany({
+        where: (goal, { eq }) => eq(goal.userId, input.userId),
+      })
+      return res
+    }),
+  get: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const res = await ctx.db.query.goals.findMany({
+        where: (goal, { eq }) => eq(goal.id, input.id),
+      })
+      return res
+    }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        title: z.string(),
+        description: z.string(),
+        state: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const trainerId = ctx.session?.user.id
+      const res = await ctx.db
+        .insert(goals)
+        .values({
+          userId: input.userId,
+          title: input.title,
+          description: input.description,
+          state: input.state,
+          trainerId: trainerId,
+        })
+        .returning({ id: goals.id })
+      return res
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        title: z.string(),
+        description: z.string(),
+        state: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const res = await ctx.db
+        .update(goals)
+        .set({
+          title: input.title,
+          description: input.description,
+          state: input.state,
+        })
+        .where(eq(goals.id, input.id))
+      return res
+    }),
+  delete: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      const res = await ctx.db.delete(goals).where(eq(goals.id, input.id))
+      return res
+    }),
+})
