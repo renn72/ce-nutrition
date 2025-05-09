d323b5f7d2d19949f8c8c89b8b21bfb592042b6c
renn <renn@warner.systems>
2025-05-09 13:45:22 +1000

    fix api call

--- src/app/admin/user-info/user-info.tsx
+++ src/app/admin/user-info/user-info.tsx
@@ -10,10 +10,12 @@ import { UserWeight } from './user-weight'
 const UserInfo = ({ userId }: { userId: string }) => {
   const { data: user } = api.user.get.useQuery(userId)
   const { data: dailyLogs } = api.dailyLog.getAllUser.useQuery(userId)
-  const { data: userGoals } = api.goal.getUser.useQuery({ userId })
+  const { data: userGoals, isLoading : userGoalsLoading } = api.goal.getUser.useQuery({ userId : userId})
 
   if (!user) return null
   if (!dailyLogs) return null
+  if (userGoalsLoading) return null
+
   return (
     <div className='p-4 flex flex-wrap gap-4 w-full'>
       <UserWeight
