a66493a218d039eb8d5446383481a85398470d0a
renn <renn@warner.systems>
2025-05-26 17:32:09 +1000

    update log vew

--- src/app/user/admin-logs/page.tsx
+++ src/app/user/admin-logs/page.tsx
@@ -2,95 +2,123 @@
 
 import { api } from '@/trpc/react'
 
+import { useState } from 'react'
+
+import { useClientMediaQuery } from '@/hooks/use-client-media-query'
+import { cn } from '@/lib/utils'
 import { RefreshCcw, XCircle } from 'lucide-react'
 
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from '@/components/ui/collapsible'
+import { Input } from '@/components/ui/input'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
 } from '@/components/ui/popover'
-import {useState} from 'react'
 import { Switch } from '@/components/ui/switch'
 
 export default function AdminLogs() {
-  const [ isHideMe, setIsHideMe ] = useState(false)
-  const ctx = api.useUtils()
-  const { data: logs } = api.user.getAdminLogs.useQuery()
-  const { mutate: deleteLog } = api.user.deleteAdminLog.useMutation({
-    onSuccess: () => {
-      ctx.user.invalidate()
-    },
-    onSettled: () => {
-      ctx.user.invalidate()
-    },
-  })
+	const [isHideMe, setIsHideMe] = useState(false)
+	const [filter, setFilter] = useState('')
+	const ctx = api.useUtils()
+	const { data: logs } = api.user.getAdminLogs.useQuery(undefined, {
+		refetchInterval: 1000 * 60 * 5,
+	})
+	const { mutate: deleteLog } = api.user.deleteAdminLog.useMutation({
+		onSuccess: () => {
+			ctx.user.invalidate()
+		},
+		onSettled: () => {
+			ctx.user.invalidate()
+		},
+	})
 
-  return (
-    <div className='flex flex-col gap-2 mt-16 w-[100vw] px-1 '>
-      <div className='flex items-center justify-around gap-8 w-full'>
-        <Switch
-          checked={isHideMe}
-          onCheckedChange={(checked) => {
-            setIsHideMe(checked)
-          }}
-        />
-        <RefreshCcw
-          size={20}
-          className='cursor-pointer text-primary/50 hover:text-primary active:scale-90 transition-transform cursor-pointer'
-          onClick={() => {
-            ctx.user.invalidate()
-          }}
-        />
-      </div>
-      {logs
-        ?.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-        .filter((log) => {
-          if (isHideMe) return log.user?.toLowerCase() !== 'david warner'
-          return true
-        })
-        .map((log) => (
-          <div
-            className='flex flex-col gap-0 w-full text-xs '
-            key={log.id}
-          >
-            <div className='flex justify-between items-center'>
-              <div>
-                {log.createdAt.toLocaleString('en-AU', {
-                  year: 'numeric',
-                  month: 'short',
-                  day: 'numeric',
-                  weekday: 'short',
-                  hour: 'numeric',
-                  minute: 'numeric',
-                })}
-              </div>
-              <XCircle
-                size={20}
-                className='cursor-pointer text-primary/50 hover:text-primary active:scale-90 transition-transform cursor-pointer'
-                onClick={() => {
-                  deleteLog(log.id)
-                }}
-              />
-            </div>
-            <div className='flex gap-2 items-center'>
-              <div>{log.user}</div>
-              <div>{log.task}</div>
-            </div>
-            <Popover>
-              <PopoverTrigger asChild>
-                <div className='truncate'>{log.notes}</div>
-              </PopoverTrigger>
-              <PopoverContent
-                align='start'
-                sideOffset={-44}
-                alignOffset={-44}
-                className='w-screen break-words'
-              >
-                {log.notes}
-              </PopoverContent>
-            </Popover>
-          </div>
-        ))}
-    </div>
-  )
+	const isMobile = useClientMediaQuery('(max-width: 600px)')
+
+	return (
+		<div className='flex flex-col gap-0 my-16 px-1 w-full tracking-tight md:tracking-normal '>
+			<div className='flex items-center gap-8 w-full py-2'>
+				<Switch
+					checked={isHideMe}
+					onCheckedChange={(checked) => {
+						setIsHideMe(checked)
+					}}
+				/>
+				<Input
+					value={filter}
+					onChange={(e) => {
+						setFilter(e.target.value)
+					}}
+					placeholder='filter'
+					className='w-48'
+				/>
+				<RefreshCcw
+					size={20}
+					className='cursor-pointer text-primary/50 hover:text-primary active:scale-90 transition-transform cursor-pointer'
+					onClick={() => {
+						ctx.user.invalidate()
+					}}
+				/>
+			</div>
+			{logs
+				?.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+				.filter((log) => {
+					if (isHideMe) return log.user?.toLowerCase() !== 'david warner'
+					return true
+				})
+				.filter((log) => log.task?.toLowerCase().includes(filter.toLowerCase()))
+				.map((log) => (
+					<Collapsible key={log.id}>
+						<CollapsibleTrigger asChild>
+							<div
+								className={cn(
+									'grid grid-cols-9 gap-1 text-[0.7rem] md:text-xs max-w-screen-xl py-[2px] shrink-0 ',
+									log.createdAt.getDate() % 2 === 0
+										? 'bg-primary/10'
+										: 'bg-primary/5',
+								)}
+							>
+								<div className='col-span-2 truncate'>
+									{log.createdAt.toLocaleString('en-AU', {
+										year: 'numeric',
+										month: 'numeric',
+										day: 'numeric',
+									})}
+								</div>
+								<div className='col-span-2 truncate'>{log.user}</div>
+								<div className='col-span-3 truncate'>{log.task}</div>
+								<div className='truncate col-span-2'>{log.notes}</div>
+							</div>
+						</CollapsibleTrigger>
+						<CollapsibleContent className='p-0 '>
+							<div
+								className={cn(
+									'flex flex-col gap-1 text-[0.7rem] md:text-xs max-w-screen-xl py-[2px] shrink-0 pl-4 ',
+                  'bg-blue-200'
+								)}
+							>
+								<div className='col-span-2 shrink-0'>
+									{log.createdAt.toLocaleString('en-AU', {
+										year: 'numeric',
+										month: 'numeric',
+										day: 'numeric',
+										hour: 'numeric',
+										minute: 'numeric',
+										hour12: false,
+									})}
+								</div>
+								<div className='col-span-2 '>{log.user}</div>
+								<div className='col-span-3 '>{log.task}</div>
+								<div className=' col-span-2'>{log.notes}</div>
+							</div>
+
+            </CollapsibleContent>
+					</Collapsible>
+				))}
+		</div>
+	)
 }
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -63,7 +63,10 @@ const isUserRoot = async (userId: string) => {
 
 export const userRouter = createTRPCRouter({
 	getAdminLogs: protectedProcedure.query(async ({ ctx }) => {
-		const res = await ctx.db.query.log.findMany({})
+		const res = await ctx.db.query.log.findMany({
+      limit: 200,
+      orderBy: ( log, { desc }) => [desc(log.createdAt)],
+    })
 		return res
 	}),
 	deleteAdminLog: protectedProcedure
