a6095d6350075ffcf7b64ed0b1b95b8087bc0d11
renn <renn@warner.systems>
2025-06-29 11:39:07 +1000

    add weekday to logs

--- src/components/user-logs/user-logs.tsx
+++ src/components/user-logs/user-logs.tsx
@@ -4,123 +4,97 @@ import { api } from '@/trpc/react'
 
 import { useClientMediaQuery } from '@/hooks/use-client-media-query'
 import { getFormattedDate } from '@/lib/utils'
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import type { GetUserById } from '@/types'
 
 import { CheckIn } from '@/components/check-in/check-in'
 import { DailyLog } from '@/components/daily-log/daily-log'
-import type { GetUserById } from '@/types'
 
 const DailyLogs = ({
-  userId,
-  user,
-  isAdmin = false,
+	userId,
+	user,
+	isAdmin = false,
 }: {
-  user: GetUserById
-  userId: string
-  isAdmin?: boolean
+	user: GetUserById
+	userId: string
+	isAdmin?: boolean
 }) => {
-  const { data: dailyLogs } = api.dailyLog.getAllUser.useQuery(userId || '')
-  const today = new Date()
+	const { data: dailyLogs } = api.dailyLog.getAllUser.useQuery(userId || '')
+	const today = new Date()
 
-  return (
-    <div className='flex flex-col gap-0 items-center max-w-md'>
-      {Array.from({ length: 21 }).map((_, i) => {
-        const date = new Date(today.getTime() - i * 86400000)
-        const dailyLog = dailyLogs?.find(
-          (dailyLog) => dailyLog.date === date.toDateString(),
-        )
-        if (!dailyLog) return null
-        return (
-          <div
-            key={i}
-            className='flex gap-2 flex-col w-full my-2'
-          >
-            <div className='text-sm text-muted-foreground font-semibold text-center'>
-              {getFormattedDate(date)}
-            </div>
-            <DailyLog
-              currentUser={user}
-              dailyLog={dailyLog}
-              date={date}
-              isAdmin={isAdmin}
-              isLogPage={true}
-            />
-          </div>
-        )
-      })}
-    </div>
-  )
+	return (
+		<div className='flex flex-col gap-0 items-center max-w-md'>
+			{Array.from({ length: 21 }).map((_, i) => {
+				const date = new Date(today.getTime() - i * 86400000)
+				const dailyLog = dailyLogs?.find(
+					(dailyLog) => dailyLog.date === date.toDateString(),
+				)
+				if (!dailyLog) return null
+				return (
+					<div key={dailyLog.id} className='flex gap-2 flex-col w-full my-2'>
+						<div className='text-sm text-muted-foreground font-semibold text-center'>
+							{`${date.toLocaleDateString('en-AU', {
+								weekday: 'long',
+							})} ${getFormattedDate(date)}`}
+						</div>
+						<DailyLog
+							currentUser={user}
+							dailyLog={dailyLog}
+							date={date}
+							isAdmin={isAdmin}
+							isLogPage={true}
+						/>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 const CheckIns = ({ userId }: { userId: string }) => {
-  const { data: checkIns } = api.weighIn.getAllUser.useQuery(userId || '')
+	const { data: checkIns } = api.weighIn.getAllUser.useQuery(userId || '')
 
-  return (
-    <div className='flex flex-col gap-1 items-center max-w-md'>
-      {checkIns?.slice(0, 7).map((checkIn) => (
-        <div
-          key={checkIn.id}
-          className='flex gap-2 flex-col w-full'
-        >
-          <div className='text-sm text-muted-foreground font-semibold text-center'>
-            {getFormattedDate(checkIn.date)}
-          </div>
-          <CheckIn weighIn={checkIn} />
-        </div>
-      ))}
-    </div>
-  )
+	return (
+		<div className='flex flex-col gap-1 items-center max-w-md'>
+			{checkIns?.slice(0, 7).map((checkIn) => (
+				<div key={checkIn.id} className='flex gap-2 flex-col w-full'>
+					<div className='text-sm text-muted-foreground font-semibold text-center'>
+						{getFormattedDate(checkIn.date)}
+					</div>
+					<CheckIn weighIn={checkIn} />
+				</div>
+			))}
+		</div>
+	)
 }
 
 const UserLogs = ({
-  userId,
-  isAdmin = false,
+	userId,
+	isAdmin = false,
 }: {
-  userId: string
-  isAdmin?: boolean
+	userId: string
+	isAdmin?: boolean
 }) => {
-  const isMobile = useClientMediaQuery('(max-width: 600px)')
-  const { data: user } = api.user.get.useQuery(userId || '')
-  console.log('user', user)
+	const isMobile = useClientMediaQuery('(max-width: 600px)')
+	const { data: user } = api.user.get.useQuery(userId || '')
+	console.log('user', user)
 
-  if (!user) return null
+	if (!user) return null
 
-  if (isMobile) {
-    return (
-      <div className='flex flex-col gap-4 items-center my-10 '>
-        <Tabs defaultValue='log'>
-          <div className='w-full flex justify-center'>
-            <TabsList className='px-6'>
-              <TabsTrigger value='log'>Daily Log</TabsTrigger>
-              <TabsTrigger value='checkin'>Check-in</TabsTrigger>
-            </TabsList>
-          </div>
-          <TabsContent value='log'>
-            <DailyLogs
-              userId={userId}
-              user={user}
-            />
-          </TabsContent>
-          <TabsContent value='checkin'>
-            <CheckIns userId={userId} />
-          </TabsContent>
-        </Tabs>
-      </div>
-    )
-  }
-  return (
-    <div className='flex flex-col gap-4 items-center my-10 '>
-      <div className='flex gap-4 items-center'>
-        <DailyLogs
-          user={user}
-          isAdmin={isAdmin}
-          userId={userId}
-        />
-        <CheckIns userId={userId} />
-      </div>
-    </div>
-  )
+	if (isMobile) {
+		return (
+			<div className='flex flex-col gap-4 items-center mb-10 mt-2 '>
+				<DailyLogs userId={userId} user={user} />
+			</div>
+		)
+	}
+	return (
+		<div className='flex flex-col gap-4 items-center my-10 '>
+			<div className='flex gap-4 items-center'>
+				<DailyLogs user={user} isAdmin={isAdmin} userId={userId} />
+				<CheckIns userId={userId} />
+			</div>
+		</div>
+	)
 }
 
 export { UserLogs }
