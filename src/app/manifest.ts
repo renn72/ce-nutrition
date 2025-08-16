7d79fbb890cdfd618cc2782b3d68035d9599fc00
renn <renn@warner.systems>
2025-08-16 13:47:28 +1000

    fix manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,8 +2,8 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'CE Nutrition T',
-    short_name: 'CENT',
+    name: 'CE Nutrition',
+    short_name: 'CEN',
     description: 'Your Nutrition',
     start_url: '/',
     scope: '/',
