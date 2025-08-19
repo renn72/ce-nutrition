e0d22b01439aaab0805a277eaf0cff02c263333e
renn <renn@warner.systems>
2025-08-20 06:47:29 +1000

    fix header

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,39 +1,42 @@
-
 import { isMobileDevice } from '@/lib/is-mobile-server'
 
+import { User } from '@/components/auth/user'
+import { MobileFooter } from '@/components/layout/mobile-footer'
 import { MobileHeader } from '@/components/layout/mobile-header'
 
-import { MobileFooter } from '@/components/layout/mobile-footer'
-import { User } from '@/components/auth/user'
-
 const Mobile = ({
-  children,
-  isDesktop = false,
+	children,
+	isDesktop = false,
 }: {
-  children: React.ReactNode
-  isDesktop?: boolean
+	children: React.ReactNode
+	isDesktop?: boolean
 }) => {
-  return (
-    <div className='flex flex-col gap-2 w-full min-h-[100svh] relative'>
-      <div className='absolute top-1 right-1 z-100'>
-        <User />
-      </div>
-      {children}
-    </div>
-  )
+	return (
+		<div className='flex flex-col gap-2 w-full min-h-[100svh] relative'>
+			{isDesktop ? (
+				<div className='absolute top-1 right-1 z-100'>
+					<User />
+				</div>
+			) : (
+				<MobileHeader />
+			)}
+			{children}
+			{!isDesktop && <MobileFooter />}
+		</div>
+	)
 }
 
 export default async function RootLayout({
-  children,
+	children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const isMobile = await isMobileDevice()
-  if (isMobile) {
-    return <Mobile>{children}</Mobile>
-  }
+	const isMobile = await isMobileDevice()
+	if (isMobile) {
+		return <Mobile>{children}</Mobile>
+	}
 
-  return (
-    <div className='flex flex-col items-center gap-2 '>
-      <Mobile isDesktop={true}>{children}</Mobile>
-    </div>
-  )
+	return (
+		<div className='flex flex-col items-center gap-2 '>
+			<Mobile isDesktop={true}>{children}</Mobile>
+		</div>
+	)
 }
