17e113840bf839272c9b4340acc90f2361e1f9f8
renn <renn@warner.systems>
2025-08-20 11:32:05 +1000

    redirect

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useRouter, redirect } from 'next/navigation'
 
 export default function NotFound() {
   console.log('not found')
-  const router = useRouter()
-  router.replace('/')
-  return null
+  redirect('/')
 }
