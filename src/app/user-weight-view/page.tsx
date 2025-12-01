a09dcd53e8ba9d59ac7612613d85baa08b7a3c6c
David <renn@warner.systems>
2025-12-02 10:44:56 +1100

    refactory it all

--- src/app/user-weight-view/page.tsx
+++ src/app/user-weight-view/page.tsx
@@ -1,19 +1,15 @@
 'use client'
 
-import { useState } from 'react'
-import { cn } from '@/lib/utils'
 import { api } from '@/trpc/react'
 
 import { useSearchParams } from 'next/navigation'
 
-import { CircleMinus, CirclePlus, MoveDown, MoveUp } from 'lucide-react'
+import { InstaView } from '@/components/user-insta'
 
 const View = ({ userId }: { userId: string }) => {
 	const { data: skinfolds, isLoading } =
 		api.metrics.getUserSkinfolds.useQuery(userId)
 
-	const [info, setInfo] = useState('')
-	const [size, setSize] = useState(5)
 	const data = skinfolds
 		?.map((skinfold) => ({
 			value: Number(skinfold.bodyWeight?.[0]?.bodyWeight || 0),
@@ -21,65 +17,9 @@ const View = ({ userId }: { userId: string }) => {
 		}))
 		.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
 
-	const value = (data?.[1]?.value || 0) - (data?.[0]?.value || 0)
-
 	if (isLoading) return null
 
-	console.log('data', data?.slice(-2))
-	return (
-		<div className='flex flex-col justify-center items-center w-screen h-screen'>
-			<div className='flex justify-center items-center mt-24 -ml-8 font-bold text-green-600 text-[20rem]'>
-				{value > 0 ? (
-					<MoveDown className='mb-16 -mr-20 text-black' size={250} />
-				) : (
-					<MoveUp className='mb-16 -mr-20 text-black' size={250} />
-				)}
-				{Math.abs(value).toFixed(1)}kg
-			</div>
-			<div className='-mt-36 w-full font-bold text-center uppercase text-[4rem]'>
-				Body Weight
-			</div>
-			<div
-				className={cn(
-					size === 5 ? 'text-3xl' : '',
-					size === 4 ? 'text-2xl' : '',
-					size === 3 ? 'text-xl' : '',
-					size === 2 ? 'text-base' : '',
-					size === 6 ? 'text-4xl' : '',
-					size === 7 ? 'text-5xl' : '',
-					size === 8 ? 'text-6xl' : '',
-					size === 9 ? 'text-7xl' : '',
-					'font-medium w-full text-center pb-28',
-				)}
-			>
-				<div className='flex gap-2 justify-center items-center w-full hover:bg-gray-600'>
-					<input
-						className='py-2 px-24 w-min text-center hover:bg-gray-600 focus:border-none field-sizing-content focus:outline-0'
-						value={info}
-						onChange={(e) => setInfo(e.target.value)}
-					/>
-				</div>
-				<div className='flex gap-2 justify-center items-center px-24'>
-					<CirclePlus
-						onClick={() => {
-							if (size === 9) return
-							setSize(size + 1)
-						}}
-						size={60}
-						className='text-white hover:text-black'
-					/>
-					<CircleMinus
-						onClick={() => {
-							if (size === 4) return
-							setSize(size - 1)
-						}}
-						size={60}
-						className='text-white hover:text-black'
-					/>
-				</div>
-			</div>
-		</div>
-	)
+	return <InstaView data={data} type='body weight' postfixType='kg' />
 }
 
 export default function Home() {
---
+++ src/components/user-insta/index.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import { useState } from 'react'
+import { cn } from '@/lib/utils'
+
+import { CircleMinus, CirclePlus, MoveDown, MoveUp } from 'lucide-react'
+
+const InstaView = ({
+	data,
+	type,
+	postfixType,
+}: {
+	type: string
+	postfixType: string
+	data: { value: number; date: string }[] | undefined
+}) => {
+	const [info, setInfo] = useState('')
+	const [size, setSize] = useState(5)
+
+	const [value, setValue] = useState(() =>
+		Math.abs((data?.[1]?.value || 0) - (data?.[0]?.value || 0)).toFixed(1),
+	)
+
+	const [isPositive, setIsPositive] = useState(() => {
+		const i = (data?.[1]?.value || 0) - (data?.[0]?.value || 0)
+		return i > 0
+	})
+
+	const [valueType, setValueType] = useState(type)
+
+	const [postfix, setPostfix] = useState(postfixType)
+
+	return (
+		<div className='flex flex-col justify-center items-center w-screen h-screen full'>
+			<div className='flex justify-center items-center mt-24 -ml-4 font-bold text-green-600 text-[20rem]'>
+				{isPositive ? (
+					<MoveDown
+						onClick={() => setIsPositive(false)}
+						className='mb-16 -mr-16 text-black'
+						size={250}
+					/>
+				) : (
+					<MoveUp
+						onClick={() => setIsPositive(true)}
+						className='mb-16 -mr-16 text-black'
+						size={250}
+					/>
+				)}
+				<input
+					className={cn(
+						'w-min h-full text-center field-sizing-content text-[20rem] focus:outline-0',
+					)}
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+				/>
+				<input
+					value={postfix}
+					onChange={(e) => setPostfix(e.target.value)}
+					className='field-sizing-content focus:outline-0'
+				/>
+			</div>
+			<div className='-mt-36 w-full font-bold text-center uppercase text-[4rem]'>
+				<input
+					value={valueType}
+					onChange={(e) => setValueType(e.target.value)}
+					className='text-center uppercase field-sizing-content focus:outline-0'
+				/>
+			</div>
+			<div
+				className={cn(
+					size === 5 ? 'text-3xl' : '',
+					size === 4 ? 'text-2xl' : '',
+					size === 3 ? 'text-xl' : '',
+					size === 2 ? 'text-base' : '',
+					size === 6 ? 'text-4xl' : '',
+					size === 7 ? 'text-5xl' : '',
+					size === 8 ? 'text-6xl' : '',
+					size === 9 ? 'text-7xl' : '',
+					'font-medium',
+				)}
+			>
+				<input
+					value={info}
+					onChange={(e) => setInfo(e.target.value)}
+					className='py-2 px-36 text-center hover:bg-gray-600 field-sizing-content focus:outline-0'
+				/>
+				<div className='flex gap-2 justify-center items-center px-24'>
+					<CirclePlus
+						onClick={() => {
+							if (size === 9) return
+							setSize(size + 1)
+						}}
+						size={60}
+						className='text-white hover:text-black'
+					/>
+					<CircleMinus
+						onClick={() => {
+							if (size === 4) return
+							setSize(size - 1)
+						}}
+						size={60}
+						className='text-white hover:text-black'
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export { InstaView }
