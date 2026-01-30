cc24a7dfb0965cfa2a8d4566c84af9db4491bb72
David <renn@warner.systems>
2026-01-30 23:38:13 +1100

    remove cahce

--- src/app/admin/_sidebar/admin-loader.tsx
+++ src/app/admin/_sidebar/admin-loader.tsx
@@ -12,21 +12,15 @@ const AdminLoader = ({ userId }: { userId: string }) => {
 	const _plans = api.plan.getAllMy.useQuery({
 		userId: userId,
 	})
-	const { data: yourUsers, isLoading } = api.user.getAllYour.useQuery()
+	// const { data: yourUsers } = api.user.getAllYour.useQuery()
+	//
+	// const bbUsers = yourUsers
+	// 	?.filter((user) => {
+	// 		return user.category?.find((category) => category.categoryId === 3)
+	// 	})
+	// 	?.sort((a, b) => a.name?.localeCompare(b.name ?? '') ?? 0)
 
-	const bbUsers = yourUsers
-		?.filter((user) => {
-			return user.category?.find((category) => category.categoryId === 3)
-		})
-		?.sort((a, b) => a.name?.localeCompare(b.name ?? '') ?? 0)
-
-	return (
-		<>
-			{bbUsers?.map((user) => (
-				<BBLoader userId={user.id} key={user.id} />
-			))}
-		</>
-	)
+	return null
 }
 
 export { AdminLoader }
