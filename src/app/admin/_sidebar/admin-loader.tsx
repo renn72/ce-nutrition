0b89223e80a6f9b60eb8011422c22f8a2614fbf4
David <renn@warner.systems>
2026-01-30 23:02:56 +1100

    cache bb

--- src/app/admin/_sidebar/admin-loader.tsx
+++ src/app/admin/_sidebar/admin-loader.tsx
@@ -1,5 +1,10 @@
 import { api } from '@/trpc/react'
 
+const BBLoader = ({ userId }: { userId: string }) => {
+	const _ = api.user.get.useQuery(userId)
+	return <div />
+}
+
 const AdminLoader = ({ userId }: { userId: string }) => {
 	const _recipes = api.recipe.getAllUserCreated.useQuery({
 		userId: userId,
@@ -7,7 +12,21 @@ const AdminLoader = ({ userId }: { userId: string }) => {
 	const _plans = api.plan.getAllMy.useQuery({
 		userId: userId,
 	})
-	return null
+	const { data: yourUsers, isLoading } = api.user.getAllYour.useQuery()
+
+	const bbUsers = yourUsers
+		?.filter((user) => {
+			return user.category?.find((category) => category.categoryId === 3)
+		})
+		?.sort((a, b) => a.name?.localeCompare(b.name ?? '') ?? 0)
+
+	return (
+		<>
+			{bbUsers?.map((user) => (
+				<BBLoader userId={user.id} key={user.id} />
+			))}
+		</>
+	)
 }
 
 export { AdminLoader }
