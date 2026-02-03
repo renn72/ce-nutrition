aeb3780ca3ed51e8b1a14fdfc713406cac923f9e
David <renn@warner.systems>
2026-02-03 11:51:15 +1100

    desc for skinfolds

--- src/app/admin/user-skinfolds/_skinfolds-table/data-table.tsx
+++ src/app/admin/user-skinfolds/_skinfolds-table/data-table.tsx
@@ -6,26 +6,26 @@ import { useRouter } from 'next/navigation'
 
 import type { GetUserSkinfolds } from '@/types'
 import {
-  type ColumnFiltersState,
-  flexRender,
-  getCoreRowModel,
-  getFacetedRowModel,
-  getFacetedUniqueValues,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  type SortingState,
-  useReactTable,
-  type VisibilityState,
+	type ColumnFiltersState,
+	flexRender,
+	getCoreRowModel,
+	getFacetedRowModel,
+	getFacetedUniqueValues,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	type SortingState,
+	useReactTable,
+	type VisibilityState,
 } from '@tanstack/react-table'
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from '@/components/ui/table'
 
 import { DataTablePagination } from '@/components/table/data-table-pagination'
@@ -33,121 +33,114 @@ import { DataTablePagination } from '@/components/table/data-table-pagination'
 import { columns } from './columns'
 import { DataTableToolbar } from './data-table-toolbar'
 
-const DataTable = ({
-  data,
-}: {
-  data: GetUserSkinfolds
-}) => {
-  const router = useRouter()
+const DataTable = ({ data }: { data: GetUserSkinfolds }) => {
+	const router = useRouter()
 
-  const [rowSelection, setRowSelection] = React.useState({})
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({
-      createdAt: false,
-      id: false,
-    })
+	const [rowSelection, setRowSelection] = React.useState({})
+	const [columnVisibility, setColumnVisibility] =
+		React.useState<VisibilityState>({
+			createdAt: false,
+			id: false,
+		})
 
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    [],
-  )
-  const [sorting, setSorting] = React.useState<SortingState>([
-    {
-      id: 'date',
-      desc: true,
-    },
-  ])
+	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+		[],
+	)
+	const [sorting, setSorting] = React.useState<SortingState>([
+		{
+			id: 'date',
+			desc: false,
+		},
+	])
 
-  const table = useReactTable({
-    data,
-    columns,
-    state: {
-      sorting,
-      columnVisibility,
-      rowSelection,
-      columnFilters,
-    },
-    initialState: {
-      columnVisibility: {
-        createdAt: false,
-        id: false,
-      },
-      pagination: {
-        pageIndex: 0,
-        pageSize: 20,
-      },
-    },
-    enableRowSelection: true,
-    onRowSelectionChange: setRowSelection,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    onColumnVisibilityChange: setColumnVisibility,
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFacetedRowModel: getFacetedRowModel(),
-    getFacetedUniqueValues: getFacetedUniqueValues(),
-  })
+	const table = useReactTable({
+		data,
+		columns,
+		state: {
+			sorting,
+			columnVisibility,
+			rowSelection,
+			columnFilters,
+		},
+		initialState: {
+			columnVisibility: {
+				createdAt: false,
+				id: false,
+			},
+			pagination: {
+				pageIndex: 0,
+				pageSize: 20,
+			},
+		},
+		enableRowSelection: true,
+		onRowSelectionChange: setRowSelection,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		onColumnVisibilityChange: setColumnVisibility,
+		getCoreRowModel: getCoreRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFacetedRowModel: getFacetedRowModel(),
+		getFacetedUniqueValues: getFacetedUniqueValues(),
+	})
 
-  return (
-    <div className='space-y-4'>
-      <DataTableToolbar table={table} />
-      <div className='rounded-md border'>
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead
-                      key={header.id}
-                      colSpan={header.colSpan}
-                    >
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
-                          )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className='h-24 text-center'
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <DataTablePagination table={table} />
-    </div>
-  )
+	return (
+		<div className='space-y-4'>
+			<DataTableToolbar table={table} />
+			<div className='rounded-md border'>
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id} colSpan={header.colSpan}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext(),
+													)}
+										</TableHead>
+									)
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && 'selected'}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext(),
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className='h-24 text-center'
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+			<DataTablePagination table={table} />
+		</div>
+	)
 }
 
 export { DataTable }
--- src/app/admin/user-skinfolds/page.tsx
+++ src/app/admin/user-skinfolds/page.tsx
@@ -8,11 +8,11 @@ import { useSearchParams } from 'next/navigation'
 
 import { NumberInput } from '@/components/ui/number-input'
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from '@/components/ui/select'
 
 import { DataTable } from './_skinfolds-table/data-table'
@@ -22,142 +22,134 @@ import { SkinfoldForm } from './form'
 export const dynamic = 'force-dynamic'
 
 const SkinFolds = ({ userId }: { userId: string }) => {
-  const { data: userSkinfolds, isLoading } =
-    api.metrics.getUserSkinfolds.useQuery(userId)
+	const { data: userSkinfolds, isLoading } =
+		api.metrics.getUserSkinfolds.useQuery(userId)
 
-  console.log('userSkinfolds', userSkinfolds)
+	if (isLoading) return null
+	if (!userSkinfolds) return null
 
-  if (isLoading) return null
-  if (!userSkinfolds) return null
-
-  return <DataTable data={userSkinfolds} />
+	return <DataTable data={userSkinfolds} />
 }
 
 const Chart = ({ userId }: { userId: string }) => {
-  const [range, setRange] = useState(7)
-  const [selectValue, setSelectValue] = useState('bodyWeight')
-  const { data: userSkinfolds, isLoading } =
-    api.metrics.getUserSkinfolds.useQuery(userId)
+	const [range, setRange] = useState(7)
+	const [selectValue, setSelectValue] = useState('bodyWeight')
+	const { data: userSkinfolds, isLoading } =
+		api.metrics.getUserSkinfolds.useQuery(userId)
 
-  if (isLoading) return null
-  if (!userSkinfolds) return null
+	if (isLoading) return null
+	if (!userSkinfolds) return null
 
-  const data = userSkinfolds
-    .map((skinfold) => {
-      let value = null
-      if (selectValue === 'bodyWeight')
-        value = skinfold.bodyWeight?.[0]?.bodyWeight
-      if (selectValue === 'leanMass') value = skinfold.leanMass?.[0]?.leanMass
-      if (selectValue === 'bodyFat') value = skinfold.bodyFat?.[0]?.bodyFat
-      if (selectValue === 'chin') value = skinfold.chin
-      if (selectValue === 'cheek') value = skinfold.cheek
-      if (selectValue === 'lowerAbdominal') value = skinfold.lowerAbdominal
-      if (selectValue === 'pectoral') value = skinfold.pectoral
-      if (selectValue === 'biceps') value = skinfold.biceps
-      if (selectValue === 'triceps') value = skinfold.triceps
-      if (selectValue === 'subscapular') value = skinfold.subscapular
-      if (selectValue === 'midAxillary') value = skinfold.midAxillary
-      if (selectValue === 'suprailiac') value = skinfold.suprailiac
-      if (selectValue === 'umbilical') value = skinfold.umbilical
-      if (selectValue === 'lowerBack') value = skinfold.lowerBack
-      if (selectValue === 'quadriceps') value = skinfold.quadriceps
-      if (selectValue === 'hamstrings') value = skinfold.hamstrings
-      if (selectValue === 'medialCalf') value = skinfold.medialCalf
-      if (selectValue === 'knee') value = skinfold.knee
-      if (selectValue === 'shoulder') value = skinfold.shoulder
-      return {
-        value: value,
-        date: skinfold.date,
-      }
-    })
-    .map((skinfold) => ({
-      value: Number(skinfold.value),
-      date: skinfold.date,
-    }))
-    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
-    .slice(0, range)
+	const data = userSkinfolds
+		.map((skinfold) => {
+			let value = null
+			if (selectValue === 'bodyWeight')
+				value = skinfold.bodyWeight?.[0]?.bodyWeight
+			if (selectValue === 'leanMass') value = skinfold.leanMass?.[0]?.leanMass
+			if (selectValue === 'bodyFat') value = skinfold.bodyFat?.[0]?.bodyFat
+			if (selectValue === 'chin') value = skinfold.chin
+			if (selectValue === 'cheek') value = skinfold.cheek
+			if (selectValue === 'lowerAbdominal') value = skinfold.lowerAbdominal
+			if (selectValue === 'pectoral') value = skinfold.pectoral
+			if (selectValue === 'biceps') value = skinfold.biceps
+			if (selectValue === 'triceps') value = skinfold.triceps
+			if (selectValue === 'subscapular') value = skinfold.subscapular
+			if (selectValue === 'midAxillary') value = skinfold.midAxillary
+			if (selectValue === 'suprailiac') value = skinfold.suprailiac
+			if (selectValue === 'umbilical') value = skinfold.umbilical
+			if (selectValue === 'lowerBack') value = skinfold.lowerBack
+			if (selectValue === 'quadriceps') value = skinfold.quadriceps
+			if (selectValue === 'hamstrings') value = skinfold.hamstrings
+			if (selectValue === 'medialCalf') value = skinfold.medialCalf
+			if (selectValue === 'knee') value = skinfold.knee
+			if (selectValue === 'shoulder') value = skinfold.shoulder
+			return {
+				value: value,
+				date: skinfold.date,
+			}
+		})
+		.map((skinfold) => ({
+			value: Number(skinfold.value),
+			date: skinfold.date,
+		}))
+		.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+		.slice(0, range)
 
-  return (
-    <div className='flex flex-col gap-4'>
-      <SkinfoldChart
-        data={data}
-        label='label'
-      />
-      <div className='flex gap-2 justify-center w-full'>
-        <NumberInput
-          value={range}
-          setValue={setRange}
-          fixed={1}
-          scale={1}
-          postfix=''
-          isSmall={true}
-        />
-        <Select
-          value={selectValue}
-          onValueChange={(value) => {
-            setSelectValue(value)
-          }}
-        >
-          <SelectTrigger className='w-48 h-10 rounded-lg'>
-            <SelectValue placeholder='Select' />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value='bodyWeight'>Weight</SelectItem>
-            <SelectItem value='leanMass'>Lean Mass</SelectItem>
-            <SelectItem value='bodyFat'>Body Fat</SelectItem>
-            <SelectItem value='chin'>Chin</SelectItem>
-            <SelectItem value='cheek'>Cheek</SelectItem>
-            <SelectItem value='lowerAbdominal'>Lower Abdominal</SelectItem>
-            <SelectItem value='pectoral'>Pectoral</SelectItem>
-            <SelectItem value='biceps'>Biceps</SelectItem>
-            <SelectItem value='triceps'>Triceps</SelectItem>
-            <SelectItem value='subscapular'>Subscapular</SelectItem>
-            <SelectItem value='midAxillary'>Mid Axillary</SelectItem>
-            <SelectItem value='suprailiac'>Suprailiac</SelectItem>
-            <SelectItem value='umbilical'>Umbilical</SelectItem>
-            <SelectItem value='lowerBack'>Lower Back</SelectItem>
-            <SelectItem value='quadriceps'>Quadriceps</SelectItem>
-            <SelectItem value='hamstrings'>Hamstrings</SelectItem>
-            <SelectItem value='medialCalf'>Medial Calf</SelectItem>
-            <SelectItem value='knee'>Knee</SelectItem>
-            <SelectItem value='shoulder'>Shoulder</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-    </div>
-  )
+	return (
+		<div className='flex flex-col gap-4'>
+			<SkinfoldChart data={data} label='label' />
+			<div className='flex gap-2 justify-center w-full'>
+				<NumberInput
+					value={range}
+					setValue={setRange}
+					fixed={1}
+					scale={1}
+					postfix=''
+					isSmall={true}
+				/>
+				<Select
+					value={selectValue}
+					onValueChange={(value) => {
+						setSelectValue(value)
+					}}
+				>
+					<SelectTrigger className='w-48 h-10 rounded-lg'>
+						<SelectValue placeholder='Select' />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectItem value='bodyWeight'>Weight</SelectItem>
+						<SelectItem value='leanMass'>Lean Mass</SelectItem>
+						<SelectItem value='bodyFat'>Body Fat</SelectItem>
+						<SelectItem value='chin'>Chin</SelectItem>
+						<SelectItem value='cheek'>Cheek</SelectItem>
+						<SelectItem value='lowerAbdominal'>Lower Abdominal</SelectItem>
+						<SelectItem value='pectoral'>Pectoral</SelectItem>
+						<SelectItem value='biceps'>Biceps</SelectItem>
+						<SelectItem value='triceps'>Triceps</SelectItem>
+						<SelectItem value='subscapular'>Subscapular</SelectItem>
+						<SelectItem value='midAxillary'>Mid Axillary</SelectItem>
+						<SelectItem value='suprailiac'>Suprailiac</SelectItem>
+						<SelectItem value='umbilical'>Umbilical</SelectItem>
+						<SelectItem value='lowerBack'>Lower Back</SelectItem>
+						<SelectItem value='quadriceps'>Quadriceps</SelectItem>
+						<SelectItem value='hamstrings'>Hamstrings</SelectItem>
+						<SelectItem value='medialCalf'>Medial Calf</SelectItem>
+						<SelectItem value='knee'>Knee</SelectItem>
+						<SelectItem value='shoulder'>Shoulder</SelectItem>
+					</SelectContent>
+				</Select>
+			</div>
+		</div>
+	)
 }
 
 export default function Home() {
-  const searchParams = useSearchParams()
-  const userId = searchParams.get('user')
+	const searchParams = useSearchParams()
+	const userId = searchParams.get('user')
 
-  const { data: currentUserLogs, isLoading } = api.dailyLog.getAllUser.useQuery(
-    userId ?? '',
-  )
+	const { data: currentUserLogs, isLoading } = api.dailyLog.getAllUser.useQuery(
+		userId ?? '',
+	)
 
-  if (isLoading) return null
+	if (isLoading) return null
 
-  if (
-    userId === '' ||
-    userId === undefined ||
-    userId === null ||
-    userId === 'null'
-  )
-    return <div>Select a user</div>
+	if (
+		userId === '' ||
+		userId === undefined ||
+		userId === null ||
+		userId === 'null'
+	)
+		return <div>Select a user</div>
 
-  const bodyWeight = currentUserLogs?.sort((a, b) =>
-    b.date.localeCompare(a.date),
-  )[0]?.morningWeight
+	const bodyWeight = currentUserLogs?.sort((a, b) =>
+		b.date.localeCompare(a.date),
+	)[0]?.morningWeight
 
-  return (
-    <div className='max-w-[1400px] mx-auto my-10'>
-      <SkinfoldForm
-        userId={userId}
-        bodyWeight={bodyWeight || ''}
-      />
-      <Chart userId={userId} />
-      <SkinFolds userId={userId} />
-    </div>
-  )
+	return (
+		<div className='my-10 mx-auto max-w-[1400px]'>
+			<SkinfoldForm userId={userId} bodyWeight={bodyWeight || ''} />
+			<Chart userId={userId} />
+			<SkinFolds userId={userId} />
+		</div>
+	)
 }
