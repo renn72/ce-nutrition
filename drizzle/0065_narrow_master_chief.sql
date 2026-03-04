77e6e2a16cbc6a21803be4d3b4c8aa9e2545c116
David <renn@warner.systems>
2026-03-04 11:06:42 +1100

    db additions

---
+++ drizzle/0065_narrow_master_chief.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `daily_log` ADD `is_bulk` integer;--> statement-breakpoint
+ALTER TABLE `daily_log` ADD `is_cut` integer;--> statement-breakpoint
+ALTER TABLE `daily_log` ADD `is_high` integer;--> statement-breakpoint
+ALTER TABLE `daily_log` ADD `is_low` integer;
